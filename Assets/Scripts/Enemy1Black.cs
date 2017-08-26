5251b667705a3354d552c0a7d32371fac0455162
alexanderconner <astralbody888@gmail.com>
2017-08-26 16:22:44 -0400

    Created rigidbodies and colliders for lasers and enemies so they can take damage when shot.

--- Assets/Scripts/Enemy1Black.cs
+++ Assets/Scripts/Enemy1Black.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Enemy1Black : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile missile = collider.gameObject.GetComponent<Projectile> ();
+		if (missile) {
+			health -= missile.getDamage ();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		} else { Debug.Log ("Player collision"); 
+		}
 	}
 }
